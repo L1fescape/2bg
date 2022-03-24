9929423c83dd51a99023b7a355fdd3a5b7693cd5
Andrew Kennedy <akennedy2292@gmail.com>
2022-03-24 12:32:06 -0400

    init cloudflare workers

---
+++ src/handler.ts
@@ -0,0 +1,28 @@
+const html = `
+<!DOCTYPE html lang="en">
+<head>
+  <title>2bg.gg</title>
+  <style>
+    html, body {
+      background: #222;
+      color: #eee;
+      margin: 0;
+      padding: 1rem;
+      font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Helvetica,Arial,sans-serif;
+      line-height: 1.5rem;
+    }
+  </style>
+  </head>
+  <body>
+    2bg.gg
+  </body>
+</html>
+`
+
+export async function handleRequest(request: Request): Promise<Response> {
+  return new Response(html, {
+    headers: {
+      'content-type': 'text/html;charset=UTF-8',
+    },
+  })
+}
